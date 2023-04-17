3dbeab0a4e3fcb7c0d1bcfb7777cf02d1648f393
Alisa Semenova <semyonovaav@yandex.ru>
2023-04-17 15:27:57 +0300

    Initial migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(defaultConnection));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
