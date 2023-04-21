152abb5211eb187eddd2ee41ac453de0ec321cc5
Alisa Semenova <semyonovaav@yandex.ru>
2023-04-21 16:50:58 +0300

    Add identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
+using Core.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -9,10 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 var redisConnection = builder.Configuration.GetConnectionString("RedisConnection");
+var identityConnection = builder.Configuration.GetConnectionString("IdentityConnection");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppIdentityDbContext>(x => x.UseSqlite(identityConnection));
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(defaultConnection));
 builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 {
@@ -22,6 +27,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddLogging();
 builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices();
 builder.Services.AddSwaggerDocumentation();
 builder.Services.AddCors(opt =>
 {
@@ -45,6 +51,11 @@ using (var scope = app.Services.CreateScope())
         var context = services.GetRequiredService<StoreContext>();
         await context.Database.MigrateAsync();
         await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+        var userManager = services.GetRequiredService<UserManager<AppUser>>();
+        var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+        await identityContext.Database.MigrateAsync();
+        await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
     }
     catch (Exception ex)
     {
