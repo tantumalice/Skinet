cbca20df8b1bc0c78c0c6a741fd64600a7380882
Alisa Semenova <semyonovaav@yandex.ru>
2023-04-24 18:19:50 +0300

    Configured swagger to use token

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Extensions;
+﻿using Microsoft.OpenApi.Models;
+
+namespace API.Extensions;
 
 public static class SwaggerServiceExtensions
 {
@@ -6,7 +8,33 @@ public static class SwaggerServiceExtensions
     {
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                    },
+                    new string[]{ }
+                }
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer",
+            });
+        });
 
         return services;
     }
