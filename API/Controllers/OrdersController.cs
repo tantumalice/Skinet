f87c5bb70e45f697cdb6067ffd218516586f5d13
Alisa Semenova <semyonovaav@yandex.ru>
2023-04-25 17:56:59 +0300

    Add methods to orders controller

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -35,4 +35,31 @@ public class OrdersController : BaseApiController
         }
         return Ok(order);
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+        var orders = await _orderService.GetOrdersForUserAsync(email);
+        return Ok(orders);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Order>> GetOrderByIdForUser(int id)
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+        var order = await _orderService.GetOrderByIdAsync(id, email);
+
+        if (order is null)
+        {
+            return NotFound(new ApiResponse(404));
+        }
+        return order;
+    }
+
+    [HttpGet("deliveryMethods")]
+    public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+    {
+        return Ok(await _orderService.GetDeliveryMethodsAsync());
+    }
 }
