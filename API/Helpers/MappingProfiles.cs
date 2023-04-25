eeeed2900b1ceead50f71bda4f8a3c2c6b2c4d39
Alisa Semenova <semyonovaav@yandex.ru>
2023-04-25 18:42:31 +0300

    Fixed picture url

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -23,6 +23,7 @@ public class MappingProfiles : Profile
         CreateMap<OrderItem, OrderItemDto>()
             .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
             .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
-            .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl));
+            .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemUrlResolver>());
     }
 }
---
+++ API/Helpers/OrderItemUrlResolver.cs
@@ -0,0 +1,25 @@
+﻿using API.DTO;
+using AutoMapper;
+using Core.Entites.OrderAggregate;
+
+namespace API.Helpers;
+
+public class OrderItemUrlResolver : IValueResolver<OrderItem, OrderItemDto, string>
+{
+    private readonly IConfiguration _config;
+
+    public OrderItemUrlResolver(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string Resolve(OrderItem source, OrderItemDto destination,
+        string destMember, ResolutionContext context)
+    {
+        if (!string.IsNullOrEmpty(source.ItemOrdered.PictureUrl))
+        {
+            return _config["ApiUrl"] + source.ItemOrdered.PictureUrl;
+        }
+        return null;
+    }
+}
