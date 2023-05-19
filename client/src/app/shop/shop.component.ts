450e84d4256a1e532718b3bafd40ea904adf2f77
Alisa Semenova <semyonovaav@yandex.ru>
2023-05-19 12:05:29 +0300

    Moving code to modules

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  products: IProduct[];
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.shopService.getProducts().subscribe(response => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 
 }
