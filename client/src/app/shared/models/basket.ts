84c0cc8e644bb0a5eedbe95f38a5f2f48439a5a6
Alisa Semenova <semyonovaav@yandex.ru>
2023-05-24 14:33:10 +0300

    Added basket logic to client

---
+++ client/src/app/shared/models/basket.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export interface IBasket {
+    id: string
+    items: IBasketItem[]
+    deliveryMethodId: number
+    clientSecret: string
+    paymentIntentId: string
+}
+
+export interface IBasketItem {
+    id: number
+    productName: string
+    price: number
+    quantity: number
+    pictureUrl: string
+    brand: string
+    type: string
+}
+
+export class Basket implements IBasket {
+    id = uuidv4();
+    items: IBasketItem[] = [];
+    deliveryMethodId: number
+    clientSecret: string
+    paymentIntentId: string
+}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,12 +6,13 @@ import { IType } from '../shared/models/product-type';
 import { map } from 'rxjs';
 import { ShopParams } from '../shared/models/shop-params';
 import { IProduct } from '../shared/models/product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
